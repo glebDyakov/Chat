6fb0c99ac12f660b9e8491f7fa36dfb3d58755b1
glebDyakov <67467704+glebDyakov@users.noreply.github.com>
2020-12-05 13:57:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+﻿var express= require("express");
+var app= express();
+var server= require("http").createServer(app);
+var io= require("socket.io").listen(server);
+app.use(express.static('public'));
+server.listen(3000);
+app.get("/", function(request, response){
+response.sendFile(__dirname + "/index.html");
+});
+var connections=[];
+io.sockets.on("connection", function(socket){
+connections.push(socket);
+console.log("подключились");
+socket.on("disconnect",function(data){
+connections.splice(connections.indexOf(socket), 1);
+console.log("отсоединились");
+});
+socket.on("send", function(data){
+	io.sockets.emit("add_mess", {msg:data.input, name:data.name, class:data.class});
+});
+});
